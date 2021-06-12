160ce5ed825ec1af43bfb27cd8c1ef99b498b88f
gj-wes <negativegary@gmail.com>
2021-06-12 14:36:04 +0100

    form labels added and minor styling fixes

--- src/components/RegionFilterSelect.vue
+++ src/components/RegionFilterSelect.vue
@@ -1,13 +1,15 @@
 <template>
-  <select v-model="selectedRegionFilter" @change="$emit('regionFilterChanged', selectedRegionFilter)">
-    <option value="" disabled>Filter by Region</option>
-    <option value="All">All</option>
-    <option value="Africa">Africa</option>
-    <option value="Americas">Americas</option>
-    <option value="Asia">Asia</option>
-    <option value="Europe">Europe</option>
-    <option value="Oceania">Oceania</option>
-  </select>
+  <div>
+    <label id="regionFilterSelect">Filter by Region:</label>
+    <select v-model="selectedRegionFilter" @change="$emit('regionFilterChanged', selectedRegionFilter)" id="regionFilterSelect">
+      <option value="All" selected>All</option>
+      <option value="Africa">Africa</option>
+      <option value="Americas">Americas</option>
+      <option value="Asia">Asia</option>
+      <option value="Europe">Europe</option>
+      <option value="Oceania">Oceania</option>
+    </select>
+  </div>
 </template>
 
 <script>
@@ -15,13 +17,17 @@ export default {
   emits: ['regionFilterChanged'],
   data() {
     return {
-      selectedRegionFilter: ''
+      selectedRegionFilter: 'All'
     }
   }
 }
 </script>
 
 <style scoped>
+  label {
+    display: block;
+    margin-left: 1rem;
+  }
   select {
     margin: .8rem 0 .8rem 1rem;
   }
